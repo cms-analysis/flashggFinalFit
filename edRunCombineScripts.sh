fa32acbd765cca836760172ef57862f86dda0526
Ed Scott <e.scott15@ic.ac.uk>
2017-03-14 16:23:41 +0000

    single relabelling for systematics table, yields table update (naive significance still default for now)

--- edRunCombineScripts.sh
+++ edRunCombineScripts.sh
@@ -6,7 +6,6 @@ DATA="/vols/cms/es811/FinalFits/ws_900/allData.root"
 
 FILE125="/vols/cms/es811/FinalFits/ws_900/output_GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8_GG2H.root,/vols/cms/es811/FinalFits/ws_900/output_VBFHToGG_M125_13TeV_amcatnlo_pythia8_VBF.root,/vols/cms/es811/FinalFits/ws_900/output_WHToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_WH2HQQ.root,/vols/cms/es811/FinalFits/ws_900/output_ZHToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_QQ2HLL.root,/vols/cms/es811/FinalFits/ws_900/output_WHToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_QQ2HLNU.root,/vols/cms/es811/FinalFits/ws_900/output_ZHToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_ZH2HQQ.root,/vols/cms/es811/FinalFits/ws_900/output_ttHJetToGG_M125_13TeV_amcatnloFXFX_madspin_pythia8_TTH.root"
 
-#EXT="STXSreMiniAOD_11March"
 EXT="STXSreMiniAOD_11March_GainScales"
 echo "Ext is $EXT"
 PROCS="GG2H,VBF,TTH,QQ2HLL,QQ2HLNU,WH2HQQ,ZH2HQQ"
@@ -24,7 +23,6 @@ echo "Bswidth is $BSWIDTH"
 NBINS=320
 echo "Nbins is $NBINS"
 
-#SCALES="HighR9EB,HighR9EE,LowR9EB,LowR9EE"
 SCALES="HighR9EB,HighR9EE,LowR9EB,LowR9EE,Gain1EB,Gain6EB"
 SCALESCORR="MaterialCentral,MaterialForward,FNUFEE,FNUFEB,ShowerShapeHighR9EE,ShowerShapeHighR9EB,ShowerShapeLowR9EE,ShowerShapeLowR9EB"
 SCALESGLOBAL="NonLinearity:UntaggedTag_0:2,Geant4"
