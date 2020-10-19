7d47cf597c739a751258a7fb01a7ac8a2887c31a
jlangfor <jl2117@ic.ac.uk>
2020-10-19 15:55:03 +0100

    more clear with proc/cat name in simpleFit.py

--- Signal/simpleFit.py
+++ Signal/simpleFit.py
@@ -13,6 +13,8 @@ f = ROOT.TFile("/vols/cms/jl2117/hgg/ws/UL/Sept20/MC_final/signal_2016/output_Gl
 ws = f.Get("tagsDumper/cms_hgg_13TeV")
 xvar = ws.var("CMS_hgg_mass") # Could also build this manually rather than extracting from workspace (RooRealVar)
 
+processName = "GG2H_0J_PTH_0_10" #these are just used for output plot name
+categoryName = "RECO_0J_PTH_0_10_Tag0" # these are just used for output plot names
 MHLow = '120'
 MHHigh = '130'
 massPoints = '125'
@@ -26,13 +28,12 @@ nGauss = 3 #nGaussians if not using DCB
 # MH var
 MH = ROOT.RooRealVar("MH","m_{H}", int(MHLow), int(MHHigh))
 
-
 # Create dict to store datasets: key=mass point, value = ROOT.RooDataSet()
 datasets = od()
 datasets['125'] = ws.data("ggh_125_13TeV_RECO_0J_PTH_0_10_Tag0")
 
 # Build ssf object + pdfs
-ssf = SimultaneousFit("name","proc","cat",datasets,xvar.Clone(),MH,MHLow,MHHigh,massPoints,nBins,MHPolyOrder,minimizerMethod,minimizerTolerance)
+ssf = SimultaneousFit("name",processName,categoryName,datasets,xvar.Clone(),MH,MHLow,MHHigh,massPoints,nBins,MHPolyOrder,minimizerMethod,minimizerTolerance)
 if useDCB: ssf.buildDCBplusGaussian()
 else: ssf.buildNGaussians(nGauss)
 # Run fits and build mean + sigma splines
