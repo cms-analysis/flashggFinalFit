f98ed36f8ece630a495a3623ba2b40ef56219530
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-03-15 13:05:19 +0100

    start fitting

--- Signal/config_test_2017.py
+++ Signal/config_test_2017.py
@@ -5,13 +5,13 @@ _year = '2017'
 signalScriptCfg = {
   
   # Setup
-  'inputWSDir':'/vols/cms/jl2117/hgg/ws/UL/Sept20/MC_final/signal_%s'%_year,
-  'procs':'auto', # if auto: inferred automatically from filenames
+  'inputWSDir':'/afs/cern.ch/work/e/emanuele/hc/vbfhgg/cmgtools/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/python/plotter/cards/cards_fithgg/%s' % _year,
+  'procs':'ggh_120_13TeV,ggh_125_13TeV,ggh_130_13TeV', # if auto: inferred automatically from filenames
   'cats':'auto', # if auto: inferred automatically from (0) workspace
   'ext':'dcb_%s'%_year,
   'analysis':'STXS', # To specify which replacement dataset mapping (defined in ./python/replacementMap.py)
   'year':'%s'%_year, # Use 'combined' if merging all years: not recommended
-  'massPoints':'120,125,130',
+  'massPoints': '120,125,130',
 
   #Photon shape systematics  
   'scales':'HighR9EB,HighR9EE,LowR9EB,LowR9EE,Gain1EB,Gain6EB', # separate nuisance per year
@@ -20,9 +20,7 @@ signalScriptCfg = {
   'smears':'HighR9EBPhi,HighR9EBRho,HighR9EEPhi,HighR9EERho,LowR9EBPhi,LowR9EBRho,LowR9EEPhi,LowR9EERho', # separate nuisance per year
 
   # Job submission options
-  'batch':'IC', # ['condor','SGE','IC','local']
-  'queue':'hep.q'
-  #'batch':'condor', # ['condor','SGE','IC','local']
-  #'queue':'espresso',
+  'batch':'condor', # ['condor','SGE','IC','local']
+  'queue':'espresso',
 
 }
