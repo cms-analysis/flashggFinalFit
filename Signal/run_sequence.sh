0e3fd353ab19ebec1d9416d5a9b4d709002098ce
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-04-13 12:10:14 +0200

    dryRun option added to signal sequence script

--- Signal/run_sequence.sh
+++ Signal/run_sequence.sh
@@ -10,9 +10,10 @@ usage(){
     echo "-h|--help) "
     echo "-y|--year) "
     echo "-s|--step) "
+    echo "-d|--dryRun) "
 }
 # options may be followed by one colon to indicate they have a required argument
-if ! options=$(getopt -u -o s:y: -l help,step:,year: -- "$@")
+if ! options=$(getopt -u -o s:y:d -l help,step:,year:,dryRun -- "$@")
 then
 # something went wrong, getopt will put out an error message for us
 exit 1
@@ -24,6 +25,7 @@ case $1 in
 -h|--help) usage; exit 0;;
 -y|--year) YEAR=$2; shift ;;
 -s|--step) STEP=$2; shift ;;
+-d|--dryRun) DR=$2; shift ;;
 (--) shift; break;;
 (-*) usage; echo "$0: error - unrecognized option $1" 1>&2; usage >> /dev/stderr; exit 1;;
 (*) break;;
@@ -37,12 +39,17 @@ if [[ $YEAR < 2016 || $YEAR>2018 ]]; then
     exit
 fi
 
+DROPT=""
+if [[ $DR ]]; then
+    DROPT=" --printOnly "
+fi
+
 if [[ $STEP == "fTest" ]]; then
-    python RunSignalScripts.py --inputConfig config_test_${YEAR}.py --mode fTest --modeOpts "--xvar dipho_mass --doPlots --outdir plots --nProcsToFTest -1" --printOnly 
+    python RunSignalScripts.py --inputConfig config_test_${YEAR}.py --mode fTest --modeOpts "--xvar dipho_mass --doPlots --outdir plots --nProcsToFTest -1" ${DROPT}
 elif [[ $STEP == 'signalFit' ]]; then
-    python RunSignalScripts.py --inputConfig config_test_${YEAR}.py --mode signalFit --modeOpts="--skipSystematics --xvar dipho_mass --doPlots --outdir plots" --printOnly
+    python RunSignalScripts.py --inputConfig config_test_${YEAR}.py --mode signalFit --modeOpts="--skipSystematics --xvar dipho_mass --doPlots --outdir plots" ${DROPT}
 elif [[ $STEP == 'packager' ]]; then
-    python RunPackager.py --cats auto --inputWSDir cards/cards_fithgg/${YEAR}/ --exts test1_${YEAR} --mergeYears --printOnly
+    python RunPackager.py --cats auto --inputWSDir cards/cards_fithgg/${YEAR}/ --exts test1_${YEAR} --mergeYears ${DROPT}
 elif [[ $STEP == 'plotter' ]]; then
     python RunPlotter.py --procs all --cats all --years ${YEAR} --ext packaged --outdir plots
 else
