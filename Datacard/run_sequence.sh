ef62b166c81ce74c096f03f536ce4efc029e1d30
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-04-14 17:15:51 +0200

    add datacard sequence example

---
+++ Datacard/run_sequence.sh
@@ -0,0 +1,2 @@
+python RunYields.py --cats auto --inputWSDirMap 2016=cards/cards_current/signal_2016,2017=cards/cards_current/signal_2017,2018=cards/cards_current/signal_2018 --procs auto --ext 2022-04-12 --batch condor --queue espresso --printOnly
+
