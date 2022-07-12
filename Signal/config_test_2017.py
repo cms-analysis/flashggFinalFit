345b13e2abb63aa901f4dc372c0280dbd2560c14
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-07-12 15:05:37 +0200

    update of the fit variable in config files

--- Signal/config_test_2017.py
+++ Signal/config_test_2017.py
@@ -5,20 +5,20 @@ _year = '2017'
 signalScriptCfg = {
   
     # Setup
-    'inputWSDir':'cards/cards_current/signal_%s' % _year,
+    'inputWSDir':'trees/signal_%s' % _year,
     'procs':'auto', # if auto: inferred automatically from filenames
     'cats':'auto', # if auto: inferred automatically from (0) workspace
-    'ext':'2022-04-12_year%s'%_year,
+    'ext':'2022-07-12_year%s'%_year,
     'analysis':'AC', # To specify which replacement dataset mapping (defined in ./python/replacementMap.py)
     'year':'%s'%_year, # Use 'combined' if merging all years: not recommended
     'massPoints': '120,125,130',
-    'xvar': 'dipho_mass',
-    'outdir': 'plots',
+    'xvar': 'CMS_hgg_mass',
+    'outdir': 'plots', 
 
     #Photon shape systematics  
     'scales':'HighR9EB,HighR9EE,LowR9EB,LowR9EE,Gain1EB,Gain6EB', # separate nuisance per year
     'scalesCorr':'MaterialCentralBarrel,MaterialOuterBarrel,MaterialForward,FNUFEE,FNUFEB,ShowerShapeHighR9EE,ShowerShapeHighR9EB,ShowerShapeLowR9EE,ShowerShapeLowR9EB', # correlated across years
-    'scalesGlobal':'NonLinearity,Geant4', # affect all processes equally, correlated across years
+    'scalesGlobal': 'NonLinearity,Geant4', # affect all processes equally, correlated across years
     'smears':'HighR9EBPhi,HighR9EBRho,HighR9EEPhi,HighR9EERho,LowR9EBPhi,LowR9EBRho,LowR9EEPhi,LowR9EERho', # separate nuisance per year
 
     # Job submission options
