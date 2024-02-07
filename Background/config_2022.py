9e215af217d34d91bd10d5f8a3b070912dbf06d1
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2024-02-07 16:01:24 +0100

    Adding the config for 2022 (combined mode) for Background fits

---
+++ Background/config_2022.py
@@ -0,0 +1,16 @@
+# Config file: options for signal fitting
+
+backgroundScriptCfg = {
+  
+  # Setup
+  'inputWSDir':'../input_output_2022/ws', # location of 'allData.root' file
+  'cats':'auto', # auto: automatically inferred from input ws
+  'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
+  'ext':'earlyAnalysis', # extension to add to output directory
+  'year':'combined', # Use combined when merging all years in category (for plots)
+
+  # Job submission options
+  'batch':'condor', # [condor,SGE,IC,local]
+  'queue':'hep.q' # for condor e.g. microcentury
+  
+}
