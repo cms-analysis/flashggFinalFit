d404ee40f4c69324eaf5567d9266103c400a8d30
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2024-02-20 16:31:27 +0100

    Adding signal config python scripts (2022 earlyAnalysis)

---
+++ Signal/config_2022preEE.py
@@ -0,0 +1,24 @@
+# Config file: options for signal fitting
+signalScriptCfg = {
+  
+  # Setup
+  'inputWSDir':'../input_output_2022preEE/ws_signal/', # dir storing flashgg workspaces
+  #'inputWSDir':'/net/scratch_cms3a/spaeh/private/PhD/analyses/early_Run3_Hgg/fitting/CMSSW_10_2_13/src/flashggFinalFit/input_output_freeze/ws_signal/',
+  'procs':'auto', # if auto: inferred automatically from filenames (requires names to be of from *pythia8_{PROC}.root)
+  'cats':'auto', # if auto: inferred automatically from (0) workspace
+  #'ext':'earlyAnalysis_freeze_include', # output directory extension
+  'ext':'earlyAnalysis_2022preEE', # output directory extension
+  'analysis':'earlyAnalysis', # To specify replacement dataset and XS*BR mapping (defined in ./tools/replacementMap.py and ./tools/XSBRMap.py respectively)
+  'year':'2022preEE', # Use 'combined' if merging all years: not recommended
+  'massPoints':'120,125,130', # You can now run with a single mass point if necessary
+
+  #Photon shape systematics  
+  'scales':'scale', # separate nuisance per year
+  'scalesCorr':'', # correlated across years
+  'scalesGlobal':'', # affect all processes equally, correlated across years
+  'smears':'smearing', # separate nuisance per year
+
+  # Job submission options
+  'batch':'condor', # ['condor','SGE','IC','local']
+  'queue':'espresso' # use hep.q for IC
+}
