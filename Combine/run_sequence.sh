fb1752a7890878d575de1e6826a777b1af374719
fderiggi <federica.deriggi@cern.ch>
2024-03-28 11:57:32 +0100

    changes in inputs.json and run sequence.sh adding also dir  runImpactsALT_0M_ALT_0M

--- Combine/run_sequence.sh
+++ Combine/run_sequence.sh
@@ -34,7 +34,9 @@ if [[ $DR ]]; then
     DROPT=" --dryRun "
 fi
 
-fits=("xsec" "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M")
+fits=( "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M")
+#fits=("ALT_L1" "ALT_L1Zg" "ALT_0PH" )
+fits=("ALT_L1Zg" "ALT_0PH" )
 
 if [[ $STEP == "t2w" ]]; then
     for fit in ${fits[*]}
@@ -64,6 +66,40 @@ elif [[ $STEP == "plot" ]]; then
         do
             python PlotScans.py --inputJson inputs.json --mode $fit  --ext $fit --outdir $outdate-fits $obs
         done
+    done
+elif [[ $STEP == "impacts-initial" ]]; then
+    for fit in ${fits[*]} 
+    do
+	python RunImpacts.py --inputJson inputs.json --ext $fit --mode $fit --queue workday   ${DROPT}
+    done
+elif [[ $STEP == "impacts-scans" ]]; then
+    for fit in ${fits[*]}
+    do
+	python RunImpacts.py --inputJson inputs.json --ext $fit --mode $fit --doFits  --queue tomorrow  ${DROPT}
+    done
+elif [[ $STEP == "impacts-collect" ]]; then
+    for fit in ${fits[*]}
+    do
+	cd runImpacts${fit}_${fit}
+	echo "Making JSON file for fit $fit It might take time, depending on the number of parameters..."
+	##combineTool.py -M Impacts -n _bestfit_syst_${fit}_initialFit -d ../Datacard_${fit}.root -i impacts_${fit}.json -m 125.38 -o impacts_${poi}.json
+	if [[ $fit == "xsec" ]]; then 
+	    pois=("r_ggH" "r_VBF" "r_VH" "r_top")
+	    translate="pois_mu.json"
+   else 
+       pois=("CMS_zz4l_fai1")
+       translate="pois_fa3.json"
+	fi
+	for poi in ${pois[*]}
+	do
+
+	#combineTool.py -M Impacts -n _bestfit_syst_${fit}_initialFit -d ../Datacard_${fit}.root -i impacts_${fit}.json -m 125.38 -o impacts_${poi}
+	    echo "    ===> Producing impact plots for the *** main-only *** systematics for fit: === $fit === and POI: == $poi === "
+       
+	    combineTool.py -M Impacts -n _bestfit_syst_${fit}_initialFit -d ../Datacard_${fit}.root -i impacts_${fit}.json -m 125.38 -o impacts_${poi}.json -P ${poi}
+	    plotImpacts.py   -i impacts_${poi}.json -o impacts_${poi} --POI ${poi}  --translate "../../Plots/${translate}" --max-pages 3
+	done
+	cd -
     done
 else
     echo "Step $STEP is not one among t2w,fit,plot. Exiting."
