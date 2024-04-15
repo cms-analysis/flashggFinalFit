a34baa2e6de1b13d37e373e6cfdd20909f32b1a3
federica1998 <federica.deriggi@hotmail.it>
2024-04-15 18:26:49 +0200

    changing Datacard/run_sequence.sh

--- Datacard/run_sequence.sh
+++ Datacard/run_sequence.sh
@@ -35,47 +35,66 @@ if [[ $DR ]]; then
     DROPT=" --printOnly "
 fi
 
-smprocs=("GG2H" "VBF" "TTH" "QQ2HLNU"  "QQ2HLL")
+smprocs=("GG2H" "VBF" "TTH" "WMINUSH2HQQ" "WPLUSH2HQQ"  "QQ2HLL")
 smprocs_csv=$(IFS=, ; echo "${smprocs[*]}")
-
+echo $smprocs_csv
 if [[ $STEP == "yields" ]]; then
     # for mu-simple: exclude ALT processes
-    python RunYields.py --cats "auto" --inputWSDirMap 2016preVFP=cards/signal_2016preVFP,2016postVFP=cards/signal_2016postVFP,2017=cards/signal_2017,2018=cards/signal_2018 --procs $smprocs_csv --mergeYears --doSystematics --skipZeroes --ext ${ext}_xsec --batch Rome --queue cmsan ${DROPT}
+    echo $smprocs_csv
+
+     #python RunYields.py --cats "auto" --inputWSDirMap 2016preVFP=cards/signal_2016preVFP,2016postVFP=cards/signal_2016postVFP,2017=cards/signal_2017,2018=cards/signal_2018 --procs $smprocs_csv --mergeYears --doSystematics --skipZeroes --ext ${ext}_xsec --batch Rome --queue cmsan ${DROPT}
     
     # for the single fai fits: include one ALT sample at a time
-    for altproc in "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M"
+    #for altproc in "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M";
+
+
+    for altproc in "ALT_0M" ;
     # to get the interference correctly need the SM (fa1=0), the pure BSM (fai=1) and the mixed one (fai=0.5)
     do
+
 	# for bookkeeping mistake, for VBF the files are called ALT_xxx for VBF and ALTxx for VH,TTH
 	altproc_nonvbf=`echo ${altproc} | sed 's|_||g'`
-        vbfsamples="VBF,VBF_${altproc},VBF_${altproc}f05"
-	zhsamples="QQ2HLL,ZH_${altproc_nonvbf},ZH_${altproc_nonvbf}f05ph0"
-        if [[ $altproc == "ALT_0PH" ]]; then # not all the WH alternative samples are available yet
-	    whsamples="WMINUSH2HQQ,WPLUSH2HQQ,WH_${altproc_nonvbf},WH_${altproc_nonvbf}f05ph0"
-	    whsamples="QQ2HLNU,WH_${altproc_nonvbf},WH_${altproc_nonvbf}f05ph0"
+       
+         vbfsamples="VBF,VBF_${altproc},VBF_${altproc}f05"
+    
+
+        if [[ $altproc == "ALT_0PH" ]]; then 
+	     zhsamples="QQ2HLL,ZH_${altproc_nonvbf},ZH_${altproc_nonvbf}f05ph0"
+        elif [[ $altproc == "ALT_0M" ]]; then 
+        zhsamples="QQ2HLL,ZH_${altproc_nonvbf},ZH_${altproc_nonvbf}f05ph0"
+        elif [[ $altproc == "ALT_L1" ]]; then 
+        zhsamples="QQ2HLL,ZH_ALT0L1,ZH_ALT0L1f05ph0"
         else
-            whsamples="QQ2HLNU"
+        zhsamples="QQ2HLL,ZH_ALT0L1Zg,ZH_ALT0L1Zgf05ph0"
         fi
-	if [[ $altproc == "ALT_0M" ]]; then
-	    #tthsamples="TTH,TTH_${altproc_nonvbf},TTH_${altproc_nonvbf}f05ph0"
-	    tthsamples="TTH"
-	else
-	    tthsamples="TTH"
-	fi
-	if [[ $altproc == "ALT_L1" ]] || [[ $altproc == "ALT_L1Zg" ]]; then
-	    zhsamples=`echo ${zhsamples} | sed 's|L1|0L1|g'`
-	fi
-        python RunYields.py --cats "auto" --inputWSDirMap 2016preVFP=cards/signal_2016preVFP,2016postVFP=cards/signal_2016postVFP,2017=cards/signal_2017,2018=cards/signal_2018 --procs "GG2H,$tthsamples,$vbfsamples,$whsamples,$zhsamples" --mergeYears --doSystematics --skipZeroes --ext ${ext}_${altproc} --batch Rome --queue cmsan ${DROPT}
-    done
+
+
+        if [[ $altproc == "ALT_0PH" ]]; then 
+	     whsamples="WMINUSH2HQQ,WPLUSH2HQQ,WH_ALT0PH,WH_ALT0PHf05ph0"
+        elif [[ $altproc == "ALT_0M" ]]; then 
+        whsamples="WMINUSH2HQQ,WPLUSH2HQQ,wh_ALT_0M"
+        elif [[ $altproc == "ALT_L1" ]]; then 
+        whsamples="WMINUSH2HQQ,WPLUSH2HQQ,WH_ALT0L1f05ph0,wh_ALT_L1"
+        else
+        whsamples="WMINUSH2HQQ,WPLUSH2HQQ"
+        fi
+	   
+        tthsamples="TTH"
+	
+    
+      python RunYields.py --cats "auto" --inputWSDirMap 2016preVFP=cards/signal_2016preVFP,2016postVFP=cards/signal_2016postVFP,2017=cards/signal_2017,2018=cards/signal_2018 --procs "GG2H,$tthsamples,$vbfsamples,$whsamples,$zhsamples" --mergeYears --doSystematics --skipZeroes --ext ${ext}_${altproc} --batch Rome --queue cmsan ${DROPT}
+  done
 elif [[ $STEP == "datacards" ]]; then
 #    for fit in "xsec" "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M"
-    for fit in "ALT_L1" "ALT_L1Zg" "ALT_0PH" "ALT_0M"
-    do
+    for fit in  "xsec"  
+   do
 	echo "making datacards for all years together for type of fit: $fit"
-       python makeDatacard.py --years 2016preVFP,2016postVFP,2017,2018 --ext ${ext}_${fit} --prune --doSystematics --output "Datacard_${fit}" --pruneCat RECO_VBFLIKEGGH_Tag1,RECO_VBFLIKEGGH_Tag0
+    python makeDatacard.py --years 2016preVFP,2016postVFP,2017,2018 --ext ${ext}_${fit} --prune --doSystematics --output "Datacard_${fit}" --pruneCat RECO_VBFLIKEGGH_Tag1,RECO_VBFLIKEGGH_Tag0 
 	python cleanDatacard.py --datacard "Datacard_${fit}.txt" --factor 2 --removeDoubleSided
 	mv "Datacard_${fit}_cleaned.txt" "Datacard_${fit}.txt"
+
     done
+
 elif [[ $STEP == "links" ]]; then
     cd Models 
     rm signal background 
