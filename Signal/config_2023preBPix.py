282705c709a60bde73dfe1676e18ca780cd38da9
maxwrabetz <maximilian.wrabetz@rwth-aachen.de>
2025-05-14 16:22:12 +0200

    new configs for 2023

---
+++ Signal/config_2023preBPix.py
@@ -0,0 +1,25 @@
+# Config file: options for signal fitting
+signalScriptCfg = {
+  
+  # Setup
+  'inputWSDir':'../input_output_2023preBPix/ws_signal/', # dir storing flashgg workspaces
+  'procs':'auto', # if auto: inferred automatically from filenames (requires names to be of from *pythia8_{PROC}.root)
+  'cats':'auto', # if auto: inferred automatically from (0) workspace
+  #'ext':'earlyAnalysis_freeze_include', # output directory extension
+  'ext':'earlyAnalysis_2023preBPix', # output directory extension
+  'analysis':'earlyAnalysisInOut', # To specify replacement dataset and XS*BR mapping (defined in ./tools/replacementMap.py and ./tools/XSBRMap.py respectively)
+  'year':'2023preBPix', # Use 'combined' if merging all years: not recommended
+  'massPoints':'120,125,130', # You can now run with a single mass point if necessary
+
+  #Photon shape systematics  
+  'scales':'ScaleEB,ScaleEE', # separate nuisance per year
+  'scalesCorr':'FNUF,Material', # correlated across years
+  #'scalesGlobal':'NonLinearity,Geant4', # affect all processes equally, correlated across years
+  # Removed nonLinearity for HIG-23-014 
+  'scalesGlobal':'Geant4', # affect all processes equally, correlated across years
+  'smears':'Smearing', # separate nuisance per year
+
+  # Job submission options
+  'batch':'condor', # ['condor_lxplus','condor','SGE','IC','local']
+  'queue':'microcentury' # use hep.q for IC
+}
