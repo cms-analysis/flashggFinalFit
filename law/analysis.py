25509a10153c068899c46ddde3749cd08afdb21a
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2025-06-26 14:21:43 +0200

    I had to import law_trees2ws in analyis.py, otherwise it does not work for me

--- law/analysis.py
+++ law/analysis.py
@@ -9,6 +9,7 @@ from commonObjects import *
 
 from Datacard.law_datacard import *
 from Background.law_background import *
+from Trees2WS.law_trees2ws import *
 
 # Function to safely create a directory
 def safe_mkdir(path):
