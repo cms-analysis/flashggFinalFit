b9fcbe410d9959968e56679db83b4185ea9f7b3b
Jan Lukas Spah <janlukas.spaeh@physik.rwth-aachen.de>
2025-06-27 17:15:46 +0200

    Also import signal

--- law/analysis.py
+++ law/analysis.py
@@ -10,6 +10,7 @@ from commonObjects import *
 from Datacard.law_datacard import *
 from Background.law_background import *
 from Trees2WS.law_trees2ws import *
+from Signal.law_signal import *
 
 # Function to safely create a directory
 def safe_mkdir(path):
